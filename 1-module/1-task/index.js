df591506bbb9c291bed9e2ff6c810228c2c140ec
Arkadiy Kurasov <ark495@gmail.com>
2020-10-18 15:48:40 +0300

    dun

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,21 +4,10 @@
  * @returns {number}
  */
 function factorial(n) {
- // let num = prompt("Ведите число", );
-  let a = n;
-  
-  let result = 1;
-for ( i  = 0; i < a ; i++){
-  result = result * (a - i);
-  }
-  returns(result);
-  // alert(result);
-
-//   let soncha = 1;
-//   i = 0;  //обнуляем счетчик
-// while( a  >  i ){
-//   soncha = soncha * ( a - i);
-//   i++;
-// }
-// alert(soncha);
+    let a = n;
+    let result = 1;
+      for ( i  = 0; i < a ; i++){
+        result = result * (a - i);
+        }
+        return(result);
 }
