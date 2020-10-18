57b15fef9b64c3cfe57c64d15d5257aa78f8d672
Arkadiy Kurasov <ark495@gmail.com>
2020-10-18 21:01:23 +0300

    salary

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,18 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  // ваш код...      let flag = false;
+        flag = false;
+        let numberOfElements = name.length; // считаем количество элементов в строке
+        if (numberOfElements < 4 && flag == false) {
+         flag = true;} else {
+          for ( i = 0; i < numberOfElements; i++ ){
+              if ( name[i] == ' ' && flag == false) {
+                  flag = true;
+                    } 
+                 }
+              }
+              return(flag);
 }
 
 function sayHello() {
